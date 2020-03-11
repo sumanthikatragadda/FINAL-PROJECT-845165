1a6507e27ffda373b5b7f214eb8a9dbfe82a05b8
sumanthikatragadda <sumanthi.katragadda@gmail.com>
2020-03-11 18:20:18 +0530

    11-03-2020

--- EMART-UI/src/app/Seller/seller/seller.component.ts
+++ EMART-UI/src/app/Seller/seller/seller.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-seller',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:Router) { }
 
   ngOnInit() {
   }
-
+  Logout()
+  {
+    localStorage.clear();
+    this.route.navigateByUrl("home");
+  }
 }
